3e127b7d0856df12f7a14a82cad2ffa65714cab6
kuldeep singh <kuldeepjassal178@gmail.com>
2025-04-02 09:03:41 +0530

    Add files via upload

---
+++ Music-Appl (html css js)/js/playlist.js
@@ -0,0 +1,38 @@
+let songs = [
+  {
+    name: "Famous",
+    artist: "Sidhu Moose Wala",
+    img: "one",
+    coverImg: "one",
+    audio: "Famous"
+  },
+  {
+    name: "G.O.A.T.",
+    artist: "Diljit Dosanjh",
+    img: "img02",
+    coverImg: "img02",
+    audio: "goat"
+  },
+  {
+    name: "2-Asle",
+    artist: "Navaan",
+    img: "three",
+    coverImg: "three",
+    audio: "2-Asle"
+  },
+  {
+    name: "Legend",
+    artist: "Sidhu Moose Wala",
+    img: "four",
+    coverImg: "four",
+    audio: "Legend"
+  }
+  ,
+  {
+    name: "Tusi Bhul Jo",
+    artist: "Sidhu Moose Wala",
+    img: "five",
+    coverImg: "five",
+    audio: "Tusi Bhul Jo"
+  }
+]
